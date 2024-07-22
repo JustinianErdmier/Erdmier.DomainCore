b747fe5ecf80917a098a88812cd5ba98b4e20eef
Justinian Erdmier <48148443+JustinErdmier@users.noreply.github.com>
2024-07-22 19:14:06 -0400

    feat(🎉): begin projects

---
+++ Demos/Domain/BookAggregate/Entities/Edition.cs
@@ -0,0 +1,17 @@
+﻿namespace Erdmier.DomainCore.Demos.Domain.BookAggregate.Entities;
+
+public sealed class Edition : Entity<EditionId>
+{
+    private Edition(EditionId id, int year)
+        : base(id)
+        => Year = year;
+
+    private Edition()
+    { }
+
+    public int Year { get; }
+
+    public static Edition Create(EditionId id, int year) => new(id, year);
+
+    public static Edition CreateUnique(int year) => new(EditionId.CreateUnique(), year);
+}
