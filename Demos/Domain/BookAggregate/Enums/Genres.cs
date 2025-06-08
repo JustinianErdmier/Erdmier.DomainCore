b4684883e0e54753d871de071455b4e759719e25
Justinian Erdmier <48148443+JustinErdmier@users.noreply.github.com>
2025-06-08 10:50:59 -0500

    feat(🔥): remove code
    Removed demos.

--- Demos/Domain/BookAggregate/Enums/Genres.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace Erdmier.DomainCore.Demos.Domain.BookAggregate.Enums;
-
-public enum Genres { Adventure, Biography, Children, Comedy, HistoricalFiction }
--- Demos/Domain/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-﻿global using Erdmier.DomainCore.Demos.Domain.BookAggregate.ValueObjects;
-global using Erdmier.DomainCore.Models;
-global using Erdmier.DomainCore.Models.Identities;
-
-global using Throw;
