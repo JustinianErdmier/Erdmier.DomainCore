6e55ae510456a4890684b2744c1f676aabf0f25c
Justinian Erdmier <48148443+JustinErdmier@users.noreply.github.com>
2025-06-08 11:28:43 -0500

    feat(📝): add and update documentation

--- Source/DomainCore/Models/AggregateRoot.cs
+++ Source/DomainCore/Models/AggregateRoot.cs
@@ -1,15 +1,19 @@
 ﻿namespace Erdmier.DomainCore.Models;
 
 /// <summary>Represents the root entity of an aggregate.</summary>
-/// <typeparam name="TId">The type behind the id of the aggregate root; must inherit from <see cref="AggregateRootId{TId}" />.</typeparam>
+/// <typeparam name="TId">The type behind the ID of the aggregate root; must inherit from <see cref="AggregateRootId{TId}" />.</typeparam>
 /// <typeparam name="TIdType">The underlying type defined in the <see cref="AggregateRootId{TId}" /> implementation.</typeparam>
+/// <remarks><b>NOTE:</b> <typeparamref name="TId" /> must be an implementation of <see cref="AggregateRootId{TId}" />.</remarks>
 public abstract class AggregateRoot<TId, TIdType> : Entity<TId>
     where TId : AggregateRootId<TIdType>
 {
+    /// <summary>Instantiates a new <see cref="AggregateRoot{TId,TIdType}" />.</summary>
+    /// <param name="id">The unique identifier of the instance.</param>
     protected AggregateRoot(TId id)
-        : base(id) =>
-        Id = id;
+        : base(id)
+        => Id = id;
 
+    /// <summary>Instantiates a new <see cref="AggregateRoot{TId,TIdType}" />.</summary>
     protected AggregateRoot()
     { }
 
@@ -20,8 +24,8 @@ public abstract class AggregateRoot<TId, TIdType> : Entity<TId>
         // ReSharper disable once UnusedMember.Global
         get => base.Id;
 
-    #pragma warning disable CA1061
+#pragma warning disable CA1061
         private init => base.Id = (TId)value;
-    #pragma warning restore CA1061
+#pragma warning restore CA1061
     }
 }
--- Source/DomainCore/Models/Entity.cs
+++ Source/DomainCore/Models/Entity.cs
@@ -5,37 +5,45 @@
 public abstract class Entity<TId> : IEquatable<Entity<TId>>
     where TId : ValueObject
 {
+    /// <summary>Instantiates a new <see cref="Entity{TId}" />.</summary>
+    /// <param name="id">The unique identifier of the instance.</param>
     protected Entity(TId id) => Id = id;
 
+    /// <summary>Instantiates a new <see cref="Entity{TId}" />.</summary>
     protected Entity()
     { }
 
     /// <summary>Gets the unique identifier of the entity.</summary>
-    public TId Id { get; protected init; } = default!;
+    public TId Id { get; protected init; } = null!;
 
     /// <summary>Determines whether the specified <see cref="Entity{TId}" /> is equal to the current <see cref="Entity{TId}" />.</summary>
     /// <param name="other">The <see cref="Entity{TId}" /> to compare with the current <see cref="Entity{TId}" />.</param>
-    /// <returns><c>true</c> if the specified <see cref="Entity{TId}" /> is equal to the current <see cref="Entity{TId}" />; otherwise, <c>false</c>.</returns>
+    /// <returns><c>TRUE</c> if the specified <see cref="Entity{TId}" /> is equal to the current <see cref="Entity{TId}" />; otherwise, <c>FALSE</c>.</returns>
+    /// <remarks><b>NOTE:</b> This method determines the equality by comparing the values of the <see cref="Id" />.</remarks>
     public bool Equals(Entity<TId>? other) => Equals(obj: other);
 
     /// <summary>Determines whether the specified object is equal to the current <see cref="Entity{TId}" />.</summary>
-    /// <param name="obj">The object to compare with the current <see cref="Entity{TId}" />.</param>
-    /// <returns><c>true</c> if the specified object is equal to the current <see cref="Entity{TId}" />; otherwise, <c>false</c>.</returns>
+    /// <param name="obj">The <see cref="object" /> to compare with the current <see cref="Entity{TId}" />.</param>
+    /// <returns><c>TRUE</c> if the specified <see cref="object" /> is equal to the current <see cref="Entity{TId}" />; otherwise, <c>FALSE</c>.</returns>
+    /// <remarks><b>NOTE:</b> This method determines the equality by comparing the values of the <see cref="Id" />.</remarks>
     public override bool Equals(object? obj) => obj is Entity<TId> entity && Id.Equals(entity.Id);
 
     /// <summary>Compares two <see cref="Entity{TId}" /> objects to determine if they are equal.</summary>
     /// <param name="left">The first <see cref="Entity{TId}" /> to compare.</param>
     /// <param name="right">The second <see cref="Entity{TId}" /> to compare.</param>
-    /// <returns><c>true</c> if both <see cref="Entity{TId}" /> objects are equal; otherwise, <c>false</c>.</returns>
+    /// <returns><c>TRUE</c> if both <see cref="Entity{TId}" /> objects are equal; otherwise, <c>FALSE</c>.</returns>
+    /// <remarks><b>NOTE:</b> This method determines the equality by comparing the values of the <see cref="Id" />.</remarks>
     public static bool operator ==(Entity<TId> left, Entity<TId> right) => Equals(left, right);
 
-    /// <summary>Compares two <see cref="Entity{TId}" /> objects to determine if they are not equal.</summary>
+    /// <summary>Compares two <see cref="Entity{TId}" /> objects to determine if they are <i>not</i> equal.</summary>
     /// <param name="left">The first <see cref="Entity{TId}" /> to compare.</param>
     /// <param name="right">The second <see cref="Entity{TId}" /> to compare.</param>
-    /// <returns><c>true</c> if both <see cref="Entity{TId}" /> objects are not equal; otherwise, <c>false</c>.</returns>
+    /// <returns><c>TRUE</c> if both <see cref="Entity{TId}" /> objects are <i>not</i> equal; otherwise, <c>FALSE</c>.</returns>
+    /// <remarks><b>NOTE:</b> This method determines the equality by comparing the values of the <see cref="Id" />.</remarks>
     public static bool operator !=(Entity<TId> left, Entity<TId> right) => !Equals(left, right);
 
     /// <summary>Returns the hash code for the current <see cref="Entity{TId}" />.</summary>
     /// <returns>A hash code for the current <see cref="Entity{TId}" />.</returns>
+    /// <remarks><b>NOTE:</b> The hashcode of <see cref="Id" /> is used as the hashcode for <see cref="Entity{TId}" />.</remarks>
     public override int GetHashCode() => Id.GetHashCode();
 }
