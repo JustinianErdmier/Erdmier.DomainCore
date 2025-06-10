b6f92a98e7f76a10d93a9bd072358922d41fe20c
Justinian Erdmier <48148443+JustinianErdmier@users.noreply.github.com>
2025-06-10 11:55:30 -0500

    feat(✅): add tests

---
+++ Tests/DomainCore.Tests.Models/IDs/BookId.cs
@@ -0,0 +1,14 @@
+﻿using Erdmier.DomainCore.Models.Identities;
+
+namespace Erdmier.DomainCore.Tests.Models.IDs;
+
+public sealed class BookId : EntityId<Guid>
+{
+    private BookId(Guid value)
+        : base(value)
+    { }
+
+    public static BookId Create() => new(Guid.CreateVersion7());
+
+    public static BookId Create(Guid value) => new(value);
+}
---
+++ Tests/DomainCore.Tests.Unit/EntityIdTests.cs
@@ -0,0 +1,36 @@
+﻿using Erdmier.DomainCore.Tests.Models.IDs;
+
+namespace Erdmier.DomainCore.Tests.Unit;
+
+public sealed class EntityIdTests
+{
+    [ Fact ]
+    public void Constructor_ShouldSetValue_WhenGivenAValue()
+    {
+        // Arrange
+        Guid expectedValue = Guid.CreateVersion7();
+
+        // Act
+        BookId bookId      = BookId.Create(expectedValue);
+        Guid   actualValue = bookId.Value;
+
+        // Assert
+        Assert.Equal(expectedValue, actualValue);
+    }
+
+    [ Fact ]
+    public void ToString_ReturnsValueAsString()
+    {
+        // Arrange
+        Guid   value  = Guid.NewGuid();
+        BookId bookId = BookId.Create(value);
+
+        // Act
+        string? actualValue = bookId.ToString();
+
+        // Assert
+        string expectedValue = value.ToString();
+
+        Assert.Equal(expectedValue, actualValue);
+    }
+}
