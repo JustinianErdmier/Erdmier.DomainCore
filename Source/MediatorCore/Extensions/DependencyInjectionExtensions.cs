cf364261fcdeb8326b0f3b5eb76125b5c829ced9
Justinian Erdmier <48148443+JustinianErdmier@users.noreply.github.com>
2025-06-22 14:02:27 -0500

    feat(📝): add documentation #18
    Added XML documentation to the types and their members.
    
    Signed-off-by: Justinian Erdmier <48148443+JustinianErdmier@users.noreply.github.com>

--- Source/MediatorCore/Extensions/DependencyInjectionExtensions.cs
+++ Source/MediatorCore/Extensions/DependencyInjectionExtensions.cs
@@ -2,8 +2,12 @@
 
 namespace Erdmier.DomainCore.MediatorCore;
 
+/// <summary>Provides extension methods for registering dependencies related to Domain Core Mediator's functionality.</summary>
 public static class DependencyInjectionExtensions
 {
+    /// <summary>Registers the necessary services and dependencies to support Domain Core Mediator functionality.</summary>
+    /// <param name="services">The <see cref="IServiceCollection" /> to which the domain mediator services will be added.</param>
+    /// <returns>The <see cref="IServiceCollection" /> instance with the domain mediator services registered.</returns>
     public static IServiceCollection AddDomainMediatorCore(this IServiceCollection services)
     {
         services.AddScoped<PublishDomainEventsInterceptor>();
