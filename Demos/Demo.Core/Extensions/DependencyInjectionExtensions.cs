f8627b7623088e2c7a87d0b158c605942507839b
Justinian Erdmier <48148443+JustinianErdmier@users.noreply.github.com>
2025-06-22 03:01:32 -0500

    feat(🏷️): add types #21
    Added new types to demo project for persistence.
    
    Signed-off-by: Justinian Erdmier <48148443+JustinianErdmier@users.noreply.github.com>

---
+++ Demos/Demo.Core/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Demo.Core.Extensions;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+    {
+        string connectionString = configuration[AppDbContext.ConnectionStringKey]
+                                  ?? throw new InvalidOperationException($"Connection string `{AppDbContext.ConnectionStringKey}` not found.");
+
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            if (environment.IsDevelopment())
+            {
+                options.EnableDetailedErrors();
+                options.EnableSensitiveDataLogging();
+            }
+
+            options.UseSqlServer(connectionString, sqlServerOptions => sqlServerOptions.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery));
+        });
+
+        return services;
+    }
+}
---
+++ Demos/Demo.Core/Persistence/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace Demo.Core.Persistence;
+
+public sealed class AppDbContext : DbContext
+{
+    public const string ConnectionStringKey = "ConnectionStrings:DemoProjectConnectionString";
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    { }
+
+    public DbSet<Book> Books => Set<Book>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
