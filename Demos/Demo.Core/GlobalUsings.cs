c712f0db7157ecab4c66309581b463cc7c3d3276
Justinian Erdmier <48148443+JustinianErdmier@users.noreply.github.com>
2025-06-22 02:58:15 -0500

    feat(🔧): update configuration file #21
    Add additional using statements to global usings.
    
    Signed-off-by: Justinian Erdmier <48148443+JustinianErdmier@users.noreply.github.com>

--- Demos/Demo.Core/GlobalUsings.cs
+++ Demos/Demo.Core/GlobalUsings.cs
@@ -1,7 +1,12 @@
 ﻿global using System.Diagnostics.CodeAnalysis;
 
+global using Demo.Core.Domain.BookAggregate;
 global using Demo.Core.Domain.BookAggregate.Entities;
 global using Demo.Core.Domain.BookAggregate.Events;
 global using Demo.Core.Domain.BookAggregate.ValueObjects;
+global using Demo.Core.Extensions;
+global using Demo.Core.Persistence;
 
 global using Erdmier.DomainCore.Mediator;
+
+global using Microsoft.EntityFrameworkCore;
