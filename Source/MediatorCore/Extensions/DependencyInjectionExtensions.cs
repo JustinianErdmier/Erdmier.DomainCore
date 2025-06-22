83db2d5acfc7958b1ba5ffeef9a8b240f0b8a7da
Justinian Erdmier <48148443+JustinianErdmier@users.noreply.github.com>
2025-06-22 14:02:43 -0500

    fix(🚨): fix compiler/linter warnings #18
    Signed-off-by: Justinian Erdmier <48148443+JustinianErdmier@users.noreply.github.com>

--- Source/MediatorCore/Extensions/DependencyInjectionExtensions.cs
+++ Source/MediatorCore/Extensions/DependencyInjectionExtensions.cs
@@ -3,11 +3,13 @@
 namespace Erdmier.DomainCore.MediatorCore;
 
 /// <summary>Provides extension methods for registering dependencies related to Domain Core Mediator's functionality.</summary>
+[ UsedImplicitly ]
 public static class DependencyInjectionExtensions
 {
     /// <summary>Registers the necessary services and dependencies to support Domain Core Mediator functionality.</summary>
     /// <param name="services">The <see cref="IServiceCollection" /> to which the domain mediator services will be added.</param>
     /// <returns>The <see cref="IServiceCollection" /> instance with the domain mediator services registered.</returns>
+    [ UsedImplicitly ]
     public static IServiceCollection AddDomainMediatorCore(this IServiceCollection services)
     {
         services.AddScoped<PublishDomainEventsInterceptor>();
