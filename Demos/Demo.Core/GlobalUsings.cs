e82c61983f68822e5cb9e502a1c6942efeaf6021
Justinian Erdmier <48148443+JustinianErdmier@users.noreply.github.com>
2025-06-22 02:17:46 -0500

    feat(🔧): add config file #21
    Add single file for demo project's using statements.

---
+++ Demos/Demo.Core/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using System.Diagnostics.CodeAnalysis;
+
+global using Demo.Core.Domain.BookAggregate.Entities;
+global using Demo.Core.Domain.BookAggregate.Events;
+global using Demo.Core.Domain.BookAggregate.ValueObjects;
+
+global using Erdmier.DomainCore.Mediator;
